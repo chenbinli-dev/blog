9ea37c581aaaea37673fd8618fb12765d763e124
chenbin.li <lichenbin1119@gmail.com>
2023-09-04 10:34:15 +0800

    feat: add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
     content: ["./src/**/*.{astro,tsx,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans]
+            }
+        },
     },
     plugins: [],
 }
