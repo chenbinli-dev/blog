bce28903f39239b0d26035a79ae62332fbee412d
chenbin.li <lichenbin1119@gmail.com>
2023-12-18 17:01:02 +0800

    chore: modify default config of `@tailwindcss/typography`

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Harmoney_Sans-Regular', ...defaultTheme.fontFamily.sans]
-      }
+      },
+      typography: ({theme}) => ({
+        DEFAULT: {
+          css: {
+            code: {
+              paddingRight: theme('padding[2]'),
+              paddingLeft: theme('padding[2]'),
+            },
+            'code::before': {
+              content: 'normal',
+            },
+            'code::after': {
+              content: 'normal',
+            },
+          }
+        },
+      })
     }
   },
   plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')]
