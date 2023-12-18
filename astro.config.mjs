ca6184c250334f96cdc1a285bf47ae2d5e4c192d
chenbin.li <lichenbin1119@gmail.com>
2023-12-18 17:00:03 +0800

    deps: integrate mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { remarkReadingTime } from './remark-reading-time.mjs';
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.codercoin.top',
   // Enable React to support React JSX components.
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
   markdown: {
     remarkPlugins: [remarkReadingTime]
   }
